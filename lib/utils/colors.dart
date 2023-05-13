d5c393ed45df94b32f13aca5f72b11b4e77dee14
Siddhi <siddhijadhav1216@gmail.com>
2023-05-13 15:34:49 +0530

    first commit

---
+++ lib/utils/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+
+const mobileBackgroundColour = Color.fromRGBO(0, 0, 0, 1);
+const webBackgroundColour = Color.fromRGBO(18, 18, 18, 1);
+const mobileSerchColour = Color.fromRGBO(38, 38, 38, 1);
+const blueColour = Color.fromRGBO(0, 149, 246, 1);
+const primaryColour = Colors.white;
+const secondaryColour = Colors.grey;
